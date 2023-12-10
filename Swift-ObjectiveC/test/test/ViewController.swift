288dec400a290188aaa7e8e69fa3b981b9324166
Ahmet1Bey <ahmet1bey@icloud.com>
2023-12-10 16:27:14 +0300

    testDeneme 5

---
+++ Swift-ObjectiveC/test/test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Ahmet1Bey on 10.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
